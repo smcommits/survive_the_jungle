bc520daf382d064158cee41cc08ba29a69fc4015
smcommits <sm.anas@outlook.com>
2021-05-17 02:48:31 +0530

    Fix linter error: Unused Variable

--- src/tests/Leaderboard.test.js
+++ src/tests/Leaderboard.test.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
- 
-import { getLeaderboardData, parseData, postData } from '../API/Leaderboard';
- 
+
+import { getLeaderboardData } from '../API/Leaderboard';
+
 jest.mock('axios');
- 
+
 describe('getLeaderboardData', () => {
   it('fetches successfully data from an API', async () => {
     const data = {
@@ -20,30 +20,23 @@ describe('getLeaderboardData', () => {
         ],
       },
     };
- 
+
     axios.get.mockImplementationOnce(() => Promise.resolve(data));
   });
- 
+
   it('fetches erroneously data from an API', async () => {
     const errorMessage = 'Network Error';
- 
-    axios.get.mockImplementationOnce(() =>
-      Promise.reject(new Error(errorMessage)),
-    );
+
+    axios.get.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
   });
 });
 
 describe('getLeaderboardData', () => {
   it('fails to fetch unmatched data', async () => {
     const data = undefined;
- 
+
     axios.get.mockImplementationOnce(() => Promise.resolve(data));
- 
+
     await expect(getLeaderboardData()).resolves.toEqual(data);
   });
- 
 });
-
-
-
-
